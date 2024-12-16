bd4ed61d38ddfb1c694fa8b614dc6960dfcc0a67
aaronjoseph-eaton <aaronjoseph@eaton.com>
2024-12-16 03:45:00 -0500

    solution for part 2

--- day04/xmas_word_search.py
+++ day04/xmas_word_search.py
@@ -20,17 +20,23 @@ cols = len(matrix[0]) - 1
 
 for i in range(rows):
     for j in range(cols):
-        if matrix[i][j] == "X":
-            for k in range(-1 , 2):
-                for l in range(-1, 2):
-                    for m in range(0, 4):
-                        if (i + k*m) >= 0 and (i + k*m) < rows and (j + l*m) >= 0 and (j + l*m) < cols:
+        if matrix[i][j] == "A":
+            if i >= 1 and i < rows - 1 and j >= 1 and j < cols - 1:
+                mas_1 = matrix[i-1][j-1] + matrix[i+1][j+1]
+                mas_2 = matrix[i+1][j-1] + matrix[i-1][j+1]
+
+                if (mas_1 == "SM" or mas_1 == "MS") and (mas_2 == "SM" or mas_2 == "MS"):
+                    total += 1
+            
+            # for k in range(-1 , 2):
+            #     for l in range(-1, 2):
+            #         for m in range(0, 4):
+            #             if (i + k*m) >= 0 and (i + k*m) < rows and (j + l*m) >= 0 and (j + l*m) < cols:
                             
-                            if matrix[i + k*m][j + l*m] == word[m]:
-                                if m == 3:
-                                    # print("i: ", i, " j: ", j, " k: ", k, " l: ", l, " m: ", m, " word: ", word, " matrix: ", matrix[i + k*m][j + l*m])
-                                    total += 1
-                            else:
-                                break
+            #                 if matrix[i + k*m][j + l*m] == word[m]:
+            #                     if m == 3:
+            #                         total += 1
+            #                 else:
+            #                     break
 
 print(total)                    
