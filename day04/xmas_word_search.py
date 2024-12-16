edc8559952c228622027cf06c74e2a71ad634381
aaronjoseph-eaton <aaronjoseph@eaton.com>
2024-12-16 00:14:22 -0500

    added solution for day 4 part 1

--- day04/xmas_word_search.py
+++ day04/xmas_word_search.py
@@ -5,6 +5,7 @@ file_path = "input_4_1.in"  # Replace with the actual file path
 
 total = 0
 matrix = []
+word = "XMAS"
 
 with open(file_path, "r") as file:
     for line in file:
@@ -12,11 +13,24 @@ with open(file_path, "r") as file:
 
 
 rows = len(matrix)
-cols = len(matrix[0])
+cols = len(matrix[0]) - 1
+
+# print("Rows: ", rows)
+# print("Cols: ", cols)
 
 for i in range(rows):
     for j in range(cols):
         if matrix[i][j] == "X":
-            for k in range(j - 1, j + 2):
-                for l in range(i - 1, i + 2):
-                    
+            for k in range(-1 , 2):
+                for l in range(-1, 2):
+                    for m in range(0, 4):
+                        if (i + k*m) >= 0 and (i + k*m) < rows and (j + l*m) >= 0 and (j + l*m) < cols:
+                            
+                            if matrix[i + k*m][j + l*m] == word[m]:
+                                if m == 3:
+                                    # print("i: ", i, " j: ", j, " k: ", k, " l: ", l, " m: ", m, " word: ", word, " matrix: ", matrix[i + k*m][j + l*m])
+                                    total += 1
+                            else:
+                                break
+
+print(total)                    
